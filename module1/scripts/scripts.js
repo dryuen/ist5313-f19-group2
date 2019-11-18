e9cb57cd82ca543b1ea7b5919407d99dd5c75455
iwu589 <iwu589@172.24.155.20>
2019-11-18 11:36:07 -0600

    changes

--- module1/scripts/scripts.js
+++ module1/scripts/scripts.js
@@ -186,13 +186,14 @@ function checkAllVisited()
 	var p12 = sessionStorage.getItem( "page12Visited" );
 	var p13 = sessionStorage.getItem( "page13Visited" );
 	
+		alert( p1 + ", " + p2 + ", " + p3 + ", " + p4 + ", " + p5 + ", " +
+		 p6 + ", " + p7 + ", " + p8 + ", " + p9 + ", " + p10 + ", " +p11 + ", " + p12 + ", " +  p13);
 	
 	if( p1 == "visited" && p2 == "visited" && p3 == "visited" && 
 		p4 == "visited" && p5 == "visited" && p6 == "visited" && 
 		p7 == "visited" && p8 == "visited" && p9 == "visited" && 
 		p10 == "visited" && p11 == "visited" && p12 == "visited" && p13 == "visited" )
 	{
-		alert( "quiz unhidden");
 		// unhide the quiz link
 		document.getElementById( "content-frame" ).contentWindow.document.getElementById( "quiz-link" ).style.display = "inline";
 	}
